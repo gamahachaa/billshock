0897dee6827b492fe23c46a7345a03fa8b202e71
bbaudry <bruno.baudry@salt.ch>
2023-04-12 16:53:22 +0200

    cleanup

--- prepare.bat
+++ prepare.bat
@@ -6,12 +6,8 @@ set BINDIR=%2\export\html5\bin
 echo %BINDIR%
 
 powershell -Command "cd "%BINDIR%""
-<<<<<<< HEAD
+
 powershell -Command "git --git-dir=%BINDIR%\.git --work-tree=%BINDIR% add *"
 powershell -Command "git --git-dir=%BINDIR%\.git --work-tree=%BINDIR% commit -m cleaning_for_master"
 powershell -Command "git --git-dir=%BINDIR%\.git --work-tree=%BINDIR% checkout master"
-=======
-powershell -Command "git add *"
-powershell -Command "git commit -m cleaning_for_master"
-powershell -Command "git checkout master"
->>>>>>> dev
+
